5f1cb8b2d6bfd61315eb2a50893c1783ef30254a
Abdullah Ibn Farouk <eng.magwad@gmail.com>
2020-08-05 16:10:20 +0200

    carousel

---
+++ assets/js/main.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+    $(".services-carousel").owlCarousel(
+        {
+            loop:true,
+            rtl:true,
+            nav:true,
+            margin:10,
+            responsiveClass:true,
+            responsive:{
+                0:{
+                    items:2,
+                    nav:false
+                },
+                600:{
+                    items:3,
+                    nav:false
+                },
+                1000:{
+                    items:5,
+                    nav:true,
+                    loop:true
+                }
+            }
+        }
+    );
+});
